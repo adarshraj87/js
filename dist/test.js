cc697d43381c6dff67c625914eeb5b6f20e24608
Adarsh Raj <adarsh@Adarshs-Air.home>
2017-11-10 13:22:46 +0000

    basic gulp working

--- dist/test.js
+++ dist/test.js
@@ -1,3 +1,10 @@
 let name = "adarsh";
 
-const CURRENT_VALUE = '10';
+name = "kona";
+
+let max_value = Number.MIN_VALUE;
+
+const CURRENT_VALUE = "10";
+
+
+console.log(`My name is ${name} and age is ${CURRENT_VALUE} and max value of salary is ${max_value}`);
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,6 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var eslint = require('gulp-eslint');
-gulp.task('default', function () {
-
-    /* Running eslint */
+let gulp = require('gulp');
+gulp.task('default',()=>{
 
     gulp.src('es6/**/*.js')
-        .pipe(eslint())
-        .pipe(eslint.format());
-
-    gulp.src('es6/**/*.js')
-        .pipe(babel())
         .pipe(gulp.dest('dist'))
 })
